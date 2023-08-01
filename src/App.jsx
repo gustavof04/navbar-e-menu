f39a2af80373dd3582b03d4672c873ae8e3c8088
gustavof04 <fernandes.gustavo2910@gmail.com>
2023-08-01 10:22:55 -0400

    chore: Adiciona uma largura máxima de 75rem nos parágrafos de texto

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
                 <div className="bg-white/10 backdrop-blur-md text-5xl sm:text-8xl text-slate-300 p-12 border-2 border-slate-500 shadow-2xl rounded-xl">Olá mundo!</div>
             </div>
             <div className="h-screen w-full bg-slate-950 px-8 py-8 table text-slate-400 text-2xl">
+                <div className="max-w-75rem mx-auto">
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius assumenda labore maxime nam nisi libero deleniti ratione voluptate voluptatum, cumque neque similique enim ullam ut provident, accusantium magni optio vel? Distinctio similique eum sunt, laudantium explicabo at asperiores natus quis consequatur odio veniam libero sint earum nesciunt reprehenderit dolores sit voluptatum quibusdam temporibus saepe a aperiam atque sequi repellendus! Vitae. Eaque, non! Quaerat repellat atque quidem excepturi explicabo ullam ea aperiam ad rerum consequatur expedita aliquam, architecto tempora tenetur quod labore facilis voluptas sapiente eaque odio neque quos? Dicta, quos!</p>
                     <br/>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius assumenda labore maxime nam nisi libero deleniti ratione voluptate voluptatum, cumque neque similique enim ullam ut provident, accusantium magni optio vel? Distinctio similique eum sunt, laudantium explicabo at asperiores natus quis consequatur odio veniam libero sint earum nesciunt reprehenderit dolores sit voluptatum quibusdam temporibus saepe a aperiam atque sequi repellendus! Vitae. Eaque, non! Quaerat repellat atque quidem excepturi explicabo ullam ea aperiam ad rerum consequatur expedita aliquam, architecto tempora tenetur quod labore facilis voluptas sapiente eaque odio neque quos? Dicta, quos!</p>
@@ -17,6 +18,7 @@ function App() {
                     <br/>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius assumenda labore maxime nam nisi libero deleniti ratione voluptate voluptatum, cumque neque similique enim ullam ut provident, accusantium magni optio vel? Distinctio similique eum sunt, laudantium explicabo at asperiores natus quis consequatur odio veniam libero sint earum nesciunt reprehenderit dolores sit voluptatum quibusdam temporibus saepe a aperiam atque sequi repellendus! Vitae. Eaque, non! Quaerat repellat atque quidem excepturi explicabo ullam ea aperiam ad rerum consequatur expedita aliquam, architecto tempora tenetur quod labore facilis voluptas sapiente eaque odio neque quos? Dicta, quos!</p>
                     <br/>
+                </div>
             </div>
         </div>
     )
