7313c6f70231136d325acb777f8e4de58386ddf0
gustavof04 <fernandes.gustavo2910@gmail.com>
2023-08-15 14:23:42 -0400

    chore: Adiciona animação ao hover da div olá mundo

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
         <div className="overflow-hidden">
             <Navbar />
             <div className={`w-screen h-screen flex items-center justify-center bg-fixed bg-[url('assets/bg.jpg')] bg-cover bg-center hover:bg-top grayscale ease-in-out duration-1000 text-slate-100`}>
-                <div className="bg-white/10 backdrop-blur-md text-5xl sm:text-8xl text-slate-300 p-12 border-2 border-slate-500 shadow-2xl rounded-xl select-none">Olá mundo!</div>
+                <div className="bg-white/10 backdrop-blur-md text-5xl sm:text-8xl text-slate-300 p-12 border-2 border-slate-500 shadow-2xl rounded-xl select-none lg:transition lg:duration-500 lg:hover:scale-110">Olá mundo!</div>
             </div>
             <div className="h-screen w-full bg-slate-950 px-8 py-8 table text-slate-400 text-2xl">
                 <div className="max-w-75rem mx-auto">
